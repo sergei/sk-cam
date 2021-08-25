d92b5d1b14deb988928f36fbbf49bc3fc8013a68
Sergei <spodshivalov@scmarinetech.com>
2021-08-25 14:40:31 -0700

    Add camera list view

--- sk-web-app/src/App.js
+++ sk-web-app/src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import Client from "@signalk/client";
+import React from "react";
+import LoginDialog from "./LoginDialog";
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+import {IconButton} from "@material-ui/core";
+import Cameras from "./Cameras";
 
 function App() {
+
+  // Listen to the "delta" event to get the stream data
+  let params = {
+    hostname: 'localhost',
+    port: 3000,
+    useTLS: false,
+    reconnect: true,
+    autoConnect: true,
+    notifications: false,
+    useAuthentication: false,
+    subscriptions: [
+      {
+        context: 'vessels.*',
+        subscribe: [
+          {
+            path: 'cameras',
+            policy: 'instant',
+          },
+        ],
+      },
+    ],
+  }
+
+  const username = localStorage.getItem('username');
+  const password = localStorage.getItem('password');
+
+  if( username &&  password){
+    params.username = username
+    params.password = password
+    params.useAuthentication = true
+    console.log('use cached credentials')
+  }
+
+  const [authenticated, setAuthenticated] = React.useState(false);
+  const [loginDialogOpen, setLoginDialogOpen] = React.useState(false);
+
+  const client = new Client(params)
+
+  function onLoginSubmit(username, password) {
+    client.authenticate(username, password)
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+  }
+  function onLogout() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    setAuthenticated(false)
+    setLoginDialogOpen( true)
+  }
+
+  const handleLoginClose = () => {
+    setLoginDialogOpen( false)
+  }
+
+  client.on('authenticated', (authData) => {
+    console.log('authenticated')
+    setAuthenticated(true)
+    setLoginDialogOpen( false)
+  })
+
+  // The 'authenticated' is not emitted if we specify credentials when creating the client,
+  // so listen for this event and check if we requested the authenticated connection
+  client.on('connect', (authData) => {
+    console.log('connect')
+    if( params.useAuthentication )
+      setAuthenticated(true)
+      setLoginDialogOpen( false)
+  })
+
+  client.on('error', (error) => {
+    console.log(`error ${error}`)
+    setAuthenticated(false)
+    setLoginDialogOpen( true)
+  })
+
+  function logoutButton(){
+    return(
+        <IconButton aria-label="delete" onClick={() => { onLogout() }}>
+          <ExitToAppOutlinedIcon />
+        </IconButton>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LoginDialog onLoginSubmit={onLoginSubmit} open={loginDialogOpen} handleClose={handleLoginClose}/>
+        <Cameras client={client} />
+        {authenticated ? logoutButton() : ''}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ sk-web-app/src/Cameras.js
@@ -0,0 +1,79 @@
+import React, {useEffect} from "react";
+import {IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, makeStyles} from "@material-ui/core";
+import SignalWifi4BarIcon from '@material-ui/icons/SignalWifi4Bar';
+import SignalWifi3BarIcon from '@material-ui/icons/SignalWifi3Bar';
+import SignalWifi2BarIcon from '@material-ui/icons/SignalWifi2Bar';
+import SignalWifi1BarIcon from '@material-ui/icons/SignalWifi1Bar';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    imageList: {
+        width: 500,
+        height: 450,
+    },
+    icon: {
+        color: 'rgba(255, 255, 255, 0.54)',
+    },
+}));
+
+function Cameras(props) {
+    const classes = useStyles();
+
+    const [cameras, setCameras] = React.useState([]);
+
+    useEffect(() => {
+        // Runs ONCE after initial rendering
+        props.client.on('delta', (delta) => {
+            delta.updates.forEach( update => {
+                update.values.forEach(value => {
+                    if(value.path === 'cameras'){
+                        console.log(value.value)
+                        setCameras(value.value)
+                    }
+                })
+            })
+        })
+    }, []);
+
+    const wifiIcon = (rssi) => {
+        if ( rssi > -50 )
+            return <SignalWifi4BarIcon/>
+        else if(rssi > -60 )
+            return <SignalWifi3BarIcon/>
+        else if(rssi > -70 )
+            return <SignalWifi2BarIcon/>
+        else
+            return <SignalWifi1BarIcon/>
+    }
+
+    return (
+        <div className={classes.root}>
+            <ImageList rowHeight={180} className={classes.imageList}>
+                <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div">Cameras</ListSubheader>
+                </ImageListItem>
+                {cameras.map((item) => (
+                    <ImageListItem key={item.url}>
+                        <img src={item.url+'/capture'} alt={item.id} />
+                        <ImageListItemBar
+                            title={item.id}
+                            actionIcon={
+                                <IconButton aria-label={`RSSI ${item.rssi} dBm`} className={classes.icon}>
+                                    {wifiIcon(item.rssi)}
+                                </IconButton>
+                            }
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </div>
+    )
+}
+
+export default Cameras;
